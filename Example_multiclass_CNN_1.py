112e576537aa5217250203cf3dded2ee7a52b368
anujnadig168 <anujleo123@gmail.com>
2018-07-31 15:29:47 +0530

    First commit

---
+++ Example_multiclass_CNN_1.py
@@ -0,0 +1,251 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 27 16:02:22 2018
+
+@author: Anuj
+"""
+
+import numpy as np
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Dropout, Flatten, Dense, Activation
+from keras import applications
+from keras.utils.np_utils import to_categorical
+import matplotlib.pyplot as plt
+import math
+import cv2
+from keras.preprocessing import image
+
+# dimensions of our images.
+img_width, img_height = 64, 64
+
+top_model_weights_path = 'bottleneck_fc_model.h5'
+train_data_dir = '/Users/Anuj/Desktop/Python codes/Dataset_faces/training_set'
+validation_data_dir = '/Users/Anuj/Desktop/Python codes/Dataset_faces/test_set'
+
+# number of epochs to train top model
+epochs = 2
+# batch size used by flow_from_directory and predict_generator
+batch_size = 32
+
+
+def save_bottlebeck_features():
+    # build the VGG16 network
+    model = applications.VGG16(include_top=False, weights='imagenet')
+
+    datagen = ImageDataGenerator(rescale=1. / 255)
+
+    generator = datagen.flow_from_directory(
+        train_data_dir,
+        target_size=(img_width, img_height),
+        batch_size=batch_size,
+        class_mode=None,
+        shuffle=False)
+
+    print(len(generator.filenames))
+    print(generator.class_indices)
+    print(len(generator.class_indices))
+
+    nb_train_samples = len(generator.filenames)
+    num_classes = len(generator.class_indices)
+
+    predict_size_train = int(math.ceil(nb_train_samples / batch_size))
+
+    bottleneck_features_train = model.predict_generator(
+        generator, predict_size_train)
+
+    np.save('bottleneck_features_train.npy', bottleneck_features_train)
+
+    generator = datagen.flow_from_directory(
+        validation_data_dir,
+        target_size=(img_width, img_height),
+        batch_size=batch_size,
+        class_mode=None,
+        shuffle=False)
+
+    nb_validation_samples = len(generator.filenames)
+
+    predict_size_validation = int(
+        math.ceil(nb_validation_samples / batch_size))
+
+    bottleneck_features_validation = model.predict_generator(
+        generator, predict_size_validation)
+
+    np.save('bottleneck_features_validation.npy',
+            bottleneck_features_validation)
+
+
+def train_top_model():
+    datagen_top = ImageDataGenerator(rescale=1. / 255)
+    generator_top = datagen_top.flow_from_directory(
+        train_data_dir,
+        target_size=(img_width, img_height),
+        batch_size=batch_size,
+        class_mode='categorical',
+        shuffle=False)
+
+    nb_train_samples = len(generator_top.filenames)
+    num_classes = len(generator_top.class_indices)
+
+    # save the class indices to use use later in predictions
+    np.save('class_indices.npy', generator_top.class_indices)
+
+    # load the bottleneck features saved earlier
+    train_data = np.load('bottleneck_features_train.npy')
+
+    # get the class lebels for the training data, in the original order
+    train_labels = generator_top.classes
+
+    # https://github.com/fchollet/keras/issues/3467
+    # convert the training labels to categorical vectors
+    train_labels = to_categorical(train_labels, num_classes=num_classes)
+
+    generator_top = datagen_top.flow_from_directory(
+        validation_data_dir,
+        target_size=(img_width, img_height),
+        batch_size=batch_size,
+        class_mode=None,
+        shuffle=False)
+
+    nb_validation_samples = len(generator_top.filenames)
+
+    validation_data = np.load('bottleneck_features_validation.npy')
+
+    validation_labels = generator_top.classes
+    validation_labels = to_categorical(
+        validation_labels, num_classes=num_classes)
+
+    model = Sequential()
+    model.add(Flatten(input_shape=train_data.shape[1:]))
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(num_classes, activation='softmax'))
+
+    model.compile(optimizer='rmsprop',
+                  loss='categorical_crossentropy', metrics=['accuracy'])
+
+    history = model.fit(train_data, train_labels,
+                        epochs=epochs,
+                        batch_size=batch_size,
+                        validation_data=(validation_data, validation_labels))
+
+    model.save_weights(top_model_weights_path)
+
+    (eval_loss, eval_accuracy) = model.evaluate(
+        validation_data, validation_labels, batch_size=batch_size, verbose=1)
+
+    print("[INFO] accuracy: {:.2f}%".format(eval_accuracy * 100))
+    print("[INFO] Loss: {}".format(eval_loss))
+
+    plt.figure(1)
+
+    # summarize history for accuracy
+
+    plt.subplot(211)
+    plt.plot(history.history['acc'])
+    plt.plot(history.history['val_acc'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+
+    # summarize history for loss
+
+    plt.subplot(212)
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+
+
+def predict():
+    # load the class_indices saved in the earlier step
+    class_dictionary = np.load('class_indices.npy').item()
+
+    num_classes = len(class_dictionary)
+    '''
+    # add the path to your test image below
+    cam = cv2.VideoCapture(0)   # 0 -> index of camera
+    cam.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+    cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+    s, img = cam.read()
+    if s:    # frame captured without any error
+        cv2.namedWindow("Camera ON", 1)
+        cv2.imshow("Camera ON",img)
+        cv2.waitKey(0)
+        cv2.imwrite("/Users/Anuj/Desktop/Python codes/Dataset_faces/Captured_images/Image_test.png",img) #save image
+        cam.release()
+        cv2.destroyAllWindows()
+    
+    test_image = image.load_img('/Users/Anuj/Desktop/Python codes/Dataset_faces/Captured_images/Image_test.png', target_size = (64, 64))
+    test_image = image.img_to_array(test_image)
+    test_image = np.expand_dims(test_image, axis = 0)
+    '''
+    
+    test_image = image.load_img('/Users/Anuj/Desktop/Python codes/Dataset_faces/test_set/Anuj/00156.png',target_size = (64,64))
+    test_image = image.img_to_array(test_image)
+    test_image = np.expand_dims(test_image, axis = 0)
+    # important! otherwise the predictions will be '0'
+    '''
+    '''
+    test_image = test_image / 255
+    
+    # build the VGG16 network
+    model = applications.VGG16(include_top=False, weights='imagenet')
+
+    # get the bottleneck prediction from the pre-trained VGG16 model
+    bottleneck_prediction = model.predict(test_image)
+
+    # build top model
+    model = Sequential()
+    model.add(Flatten(input_shape=bottleneck_prediction.shape[1:]))
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(num_classes, activation='softmax'))
+
+    model.load_weights(top_model_weights_path)
+
+    # use the bottleneck prediction on the top model to get the final
+    # classification
+    class_predicted = model.predict_classes(bottleneck_prediction)
+
+    probabilities = model.predict_proba(bottleneck_prediction)
+
+    inID = class_predicted[0]
+
+    inv_map = {v: k for k, v in class_dictionary.items()}
+
+    label = inv_map[inID]
+
+    # get the prediction label
+    print("Image ID: {}, Label: {}".format(inID, label))
+
+    # display the predictions with the image
+    
+    #cv2.putText(img, "Predicted: {}".format(label), (10, 30),
+               # cv2.FONT_HERSHEY_PLAIN, 1.5, (43, 99, 255), 2)
+
+    #cv2.imshow("Classified Image", img)
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
+    #cam = cv2.VideoCapture(1)
+    #cam.release()
+    
+    
+def prog_close():
+    import sys
+    print(sys.path)
+    cam = cv2.VideoCapture(1)
+    cam.release()
+    sys.tracebacklimit = 1
+    raise ValueError()
+
+save_bottlebeck_features()
+train_top_model()
+predict()
+prog_close()
+
+cv2.destroyAllWindows()
